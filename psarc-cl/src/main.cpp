4538715a9abb932657cd7354bfdd8eb40cdd868f
MilchRatchet <milchratchet@googlemail.com>
2023-12-31 15:34:38 +0100

    Added proper usage description to commandline tool.

--- psarc-cl/src/main.cpp
+++ psarc-cl/src/main.cpp
@@ -7,10 +7,17 @@
 
 int main(int argc, char* argv[]) {
   if (argc != 4) {
-    std::cout << "Usage:" << std::endl;
-    std::cout << "[pack,unpack]" << std::endl;
-    std::cout << "Input path" << std::endl;
-    std::cout << "Output path" << std::endl;
+    std::cout << "OVERVIEW: psarc-cl PSArc Interfacing Commandline Executable" << std::endl;
+    std::cout << std::endl;
+#ifdef WIN32
+    std::cout << "USAGE: psarc-cl.exe mode input-path output-path" << std::endl;
+#else
+    std::cout << "USAGE: psarc-cl mode input-path output-path" << std::endl;
+#endif
+    std::cout << std::endl;
+    std::cout << "MODE:" << std::endl;
+    std::cout << "  pack      Pack all files in a directory into a PSArc file." << std::endl;
+    std::cout << "  unpack    Unpack all files in a PSArc file into a directory." << std::endl;
     return -1;
   }
 
@@ -31,13 +38,4 @@ int main(int argc, char* argv[]) {
     std::cout << "No valid mode was specified (pack or unpack)" << std::endl;
     return -1;
   }
-
-  /*
-    PSArc::FileHandle test("Test.psarc");
-    PSArc::PSArcHandle handleOut;
-
-    handleOut.SetSerializationEndpoint(&test);
-    handleOut.SetArchive(&archive);
-    handleOut.Downsync();
-  */
 }
